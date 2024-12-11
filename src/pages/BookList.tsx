075d8c980472a7e62f8a57075157cfeefa5f96a1
asdfg030906 <s94979892@gmail.com>
2024-12-11 19:39:54 +0900

    feat: 페이지네이션 기능 추가

--- src/pages/BookList.tsx
+++ src/pages/BookList.tsx
@@ -1,17 +1,20 @@
+// src/components/BookList.tsx
 import React, { useState, useEffect } from "react";
 import { fetchBooks } from "../api/api";
-import {Book} from "../types/book.type.ts";
+import { Book } from "../types/book.type";
 
 const BookList: React.FC = () => {
     const [books, setBooks] = useState<Book[]>([]);
-
-
+    const [filteredBooks, setFilteredBooks] = useState<Book[]>([]);
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const itemsPerPage = 10;
 
     useEffect(() => {
         const getBooks = async () => {
             try {
                 const booksData = await fetchBooks();
                 setBooks(booksData);
+                setFilteredBooks(booksData);
             } catch (error) {
                 console.error("Error fetching books:", error);
             }
@@ -20,19 +23,34 @@ const BookList: React.FC = () => {
         getBooks();
     }, []);
 
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const currentBooks = filteredBooks.slice(startIndex, startIndex + itemsPerPage);
+
+    const handlePageChange = (newPage: number) => {
+        setCurrentPage(newPage);
+    };
+
     return (
-        <>
+        <div>
             <ul>
-                {books.map((book) => (
+                {currentBooks.map((book) => (
                     <li key={book.id}>
                         <h3>{book.title}</h3>
                         <p>작가: {book.author}</p>
                         <p>설명: {book.description}</p>
-                        <p>재고 수량: {book.quantity}</p>
+                        <p>수량: {book.quantity}</p>
                     </li>
                 ))}
             </ul>
-        </>
+
+            <div>
+                {Array.from({ length: Math.ceil(filteredBooks.length / itemsPerPage) }, (_, index) => (
+                    <button key={index} onClick={() => handlePageChange(index + 1)}>
+                        {index + 1}
+                    </button>
+                ))}
+            </div>
+        </div>
     );
 };
 
