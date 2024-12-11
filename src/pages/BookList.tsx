47f46b06cde1e61b2690fe81adc043c8c6b3af4c
asdfg030906 <s94979892@gmail.com>
2024-12-11 18:08:48 +0900

    feat: 책 리스트 출력

--- src/pages/BookList.tsx
+++ src/pages/BookList.tsx
@@ -1,9 +1,39 @@
-const BookList = () => {
+import React, { useState, useEffect } from "react";
+import { fetchBooks } from "../api/api";
+import {Book} from "../types/book.type.ts";
 
-    return(
+const BookList: React.FC = () => {
+    const [books, setBooks] = useState<Book[]>([]);
+
+
+
+    useEffect(() => {
+        const getBooks = async () => {
+            try {
+                const booksData = await fetchBooks();
+                setBooks(booksData);
+            } catch (error) {
+                console.error("Error fetching books:", error);
+            }
+        };
+
+        getBooks();
+    }, []);
+
+    return (
         <>
+            <ul>
+                {books.map((book) => (
+                    <li key={book.id}>
+                        <h3>{book.title}</h3>
+                        <p>작가: {book.author}</p>
+                        <p>설명: {book.description}</p>
+                        <p>재고 수량: {book.quantity}</p>
+                    </li>
+                ))}
+            </ul>
         </>
-    )
-}
+    );
+};
 
 export default BookList;
