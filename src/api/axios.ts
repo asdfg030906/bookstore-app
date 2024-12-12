0df697b7f2910f98a093bbb832e9302f862236d3
asdfg030906 <s94979892@gmail.com>
2024-12-12 19:51:17 +0900

    style 수정

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const authInstance = axios.create({
-    baseURL: "http://localhost:3001",
+    baseURL: process.env.REACT_APP_API_BASE_URL,
     timeout: 5000
 });
