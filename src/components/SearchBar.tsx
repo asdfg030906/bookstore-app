60ee8ff080422eaff89badb781c534005458d76f
asdfg030906 <s94979892@gmail.com>
2024-12-12 19:55:13 +0900

    스타일 수정

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -30,23 +30,6 @@ const SearchBar: React.FC<SearchBarProps> = ({ books, setFilteredBooks }) => {
     };
 
     return (
-        // <Wrap>
-        //     <div>
-        //         <div>
-        //             <img src={leaf}/>
-        //         </div>
-        //         <input
-        //             type="text"
-        //             placeholder="제목이나 저자를 검색해보세요."
-        //             value={searchTerm}
-        //             onChange={handleChange}
-        //         />
-        //
-        //         {/*<div>*/}
-        //         {/*    <img src={search}/>*/}
-        //         {/*</div>*/}
-        //     </div>
-        // </Wrap>
         <Wrap>
                 <input
                     type="text"
@@ -59,50 +42,6 @@ const SearchBar: React.FC<SearchBarProps> = ({ books, setFilteredBooks }) => {
 };
 
 export default SearchBar;
-//
-// const Wrap = styled.div`
-//     display: flex;
-//     align-items: center;
-//     justify-content: center;
-//     margin-top: 15px;
-//     width: 100%;
-//     border-radius: 30px;
-//         >div{
-//             width: 725px;
-//             height: 53px;
-//             display: flex;
-//             justify-content: space-between;
-//             background-color: #fff;
-//             align-items: center;
-//             border-radius: 30px;
-//             border: 2px solid #4CAF50;
-//             >input{
-//                 width: 580px;
-//                 height: 53px;
-//                 box-sizing: border-box;
-//                 margin: 0 10px;
-//                 border: none;
-//                 border-radius: 30px;
-//                 outline: none;
-//                 font-size: 18px;
-//             }
-//             >div{
-//                 background-color: #4CAF50;
-//                 display: flex;
-//                 justify-content: center;
-//                 align-items: center;
-//                 width: 45px;
-//                 height: 45px;
-//                 border-radius: 100%;
-//                 margin-right: 5px;
-//                 >img{
-//                     width: 24px;
-//                 }
-//             }
-//         }
-//
-// `
-
 
 const Wrap = styled.div`
     display: flex;
